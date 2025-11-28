f5a1d8a694c9acd16a251bb7cc02e9cfd8d75eb1
u230641 <james.lavin.2023@mumail.ie>
2025-11-28 21:45:25 +0000

    Updated frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
-	base: "/vote/",
+	base: "/vote/", // must match your repo name for GitHub Pages
 	build: {
 		outDir: "dist",
 	},
