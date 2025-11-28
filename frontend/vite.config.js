a33c3fa846c1820d2dd435cb78b56df6fa05fda9
u230641 <james.lavin.2023@mumail.ie>
2025-11-28 21:29:37 +0000

    Switch to GitHub Actions deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// IMPORTANT: GitHub Pages repo name
-const repoName = "vote";
-
 export default defineConfig({
 	plugins: [react()],
-	base: `/${repoName}/`,
+	base: "/vote/",
 	build: {
 		outDir: "dist",
 	},
