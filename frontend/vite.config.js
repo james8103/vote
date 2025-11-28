db281a65cf53df0bf661bf8a6f202015c5d59ef2
u230641 <james.lavin.2023@mumail.ie>
2025-11-28 21:07:13 +0000

    Rebuilt frontend for clean Vite + GH Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// IMPORTANT: GitHub Pages repo name
+const repoName = "vote";
+
 export default defineConfig({
 	plugins: [react()],
-	base: "/vote/",
+	base: `/${repoName}/`,
 	build: {
 		outDir: "dist",
 	},
